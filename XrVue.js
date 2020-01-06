75bfb378dec8372a977a3a461d7f9848d26581e5
Blake <lgq627628@163.com>
2020-01-07 00:11:44 +0800

    feat: 整理编译代码

--- XrVue.js
+++ XrVue.js
@@ -1,32 +1,42 @@
 let compileUtils = {
-  text(child, directiveName, value, vm) {
-    child.textContent = this.getVal(value, vm.$data)
-    child.removeAttribute('v-text')
+  text(node, expr, vm) {
+    let value = this.getVal(expr, vm)
+    this.updateMap.updateText(node, value)
    },
-  html(child, directiveName, value, vm) {
-    child.innerHTML = this.getVal(value, vm.$data)
-    child.removeAttribute('v-html')
+  html(node, expr, vm) {
+    let value = this.getVal(expr, vm)
+    this.updateMap.updateHtml(node, value)
    },
-  model(child, directiveName, value, vm) {
-    child.value = this.getVal(value, vm.$data)
-    child.removeAttribute('v-model')
+  model(node, expr, vm) {
+    let value = this.getVal(expr, vm)
+    this.updateMap.updateModel(node, value)
   },
-  bind(child, directiveName, value, vm) {
-    child.removeAttribute(`v-bind:${directiveName}`)
-    child.removeAttribute(`:${directiveName}`)
-    child.setAttribute(directiveName, this.getVal(value, vm.$data))
+  bind(node, expr, vm, bindName) {
+    let value = this.getVal(expr, vm)
+    this.updateMap.updateBind(node, value, bindName)
   },
-  on(child, directiveName, value, vm, eventName) {
-    child.addEventListener(eventName, vm.$methods[value])
-    child.removeAttribute(`v-on:${directiveName}`)
-    child.removeAttribute(`@${directiveName}`)
+  on(node, expr, vm, eventName) {
+    let f = vm.$options.methods && vm.$options.methods[expr]
+    node.addEventListener(eventName, f.bind(vm), false)
   },
-  getVal(value, data) {
-    let values = value.split('.')
-    let val = values.reduce((pre, cur, i) => {
-      return i === 0 ? pre : pre[cur]
-    }, data[values[0]])
-    return val
+  getVal(expr, vm) {
+    return expr.split('.').reduce((pre, cur) => {
+      return pre[cur]
+    }, vm.$data)
+  },
+  updateMap: {
+    updateText(node, value){
+      node.textContent = value
+    },
+    updateHtml(node, value){
+      node.innerHTML = value
+    },
+    updateModel(node, value){
+      node.value = value
+    },
+    updateBind(node, value, bindName){
+      node.setAttribute(bindName, value)
+    }
   }
 }
 class XrVue {
@@ -34,7 +44,6 @@ class XrVue {
     this.$options = options
     this.$el = this.isHtmlNode(options.el) ? options.el : document.querySelector(options.el)
     this.$data = options.data || {}
-    this.$methods = options.methods || {}
     // 第一步：数据劫持
     this.handleReactiveData()
     // 第二步：编译
@@ -50,49 +59,61 @@ class XrVue {
   compileFragment(f, vm) {
     Array.from(f.childNodes).forEach(child => {
       if (this.isHtmlNode(child)) { // 如果是标签
-        let attrs = child.attributes
-        Array.from(attrs).forEach(attr => {
-          let { name, value } = attr
-          if (this.isDirective(name)) {
-            let [, directive] = name.split('-')
-            let [directiveName, eventName] = directive.split(':')
-            compileUtils[directiveName](child, eventName, value, vm, eventName)
-          } else if (name.startsWith(':')) {
-            let [, eventName] = name.split(':')
-            compileUtils['bind'](child, eventName, value, vm)
-          } else if (name.startsWith('@')) {
-            let [, eventName] = name.split('@')
-            compileUtils['on'](child, eventName, value, vm, 'click')
-          }
-        })
-        let content = child.innerHTML
-        if (/\{\{(.*)\}\}/.test(content)) {
-          let newContent = content.replace(/\{\{(.*)\}\}/g, (...agrs) => {
-            return compileUtils.getVal(agrs[1], vm.$data)
-          })
-          child.innerHTML = newContent
-        }
-        // if (child.childNodes && child.childNodes.length) this.compileFragment(child, vm)
+        this.compileHtml(child, vm)
       } else { // 如果是文本
+        this.compileText(child, vm)
       }
+      if (child.childNodes && child.childNodes.length) this.compileFragment(child, vm)
     })
     return f
+  }
+  compileHtml(node, vm) {
+    let attrs = node.attributes
+    Array.from(attrs).forEach(attr => {
+      let { name, value } = attr
+      if (this.isDirective(name)) {
+        let [, directive] = name.split('-')
+        let [directiveName, eventName] = directive.split(':')
+        compileUtils[directiveName](node, value, vm, eventName)
+        node.removeAttribute(name)
+      } else if (this.isBindDirective(name)) {
+        let [, bindName] = name.split(':')
+        compileUtils['bind'](node, value, vm, bindName)
+        node.removeAttribute(`:${bindName}`)
+      } else if (this.isEventDirective(name)) {
+        let [, eventName] = name.split('@')
+        compileUtils['on'](node, value, vm, eventName)
+        node.removeAttribute(`@${eventName}`)
+      }
+    })
+  }
+  compileText(node, vm) {
+    let text = node.textContent
+    if (/{{(.*)}}/.test(text)) {
+      let value = text.replace(/{{(.*)}}/g, (...args) => {
+        return compileUtils.getVal(args[1], vm)
+      })
+      compileUtils.updateMap.updateText(node, value)
+    }
+  }
+  isBindDirective(str) {
+    return str.startsWith(':')
+  }
+  isEventDirective(str) {
+    return str.startsWith('@')
   }
   isDirective(str) {
     return str.startsWith('v-')
   }
   node2Fragment(el) {
     // 因为文档片段存在于内存中，并不在DOM树中，所以将子元素插入到文档片段时不会引起页面回流（对元素位置和几何上的计算）。
-    // 因此，使用文档片段通常会带来更好的性能。
     let f = document.createDocumentFragment();
-    let firstChild = el.firstChild
-    while(firstChild) {
-      f.appendChild(firstChild)
-      firstChild = el.firstChild
+    while(el.firstChild) {
+      f.appendChild(el.firstChild)
     }
     return f
   }
-  isHtmlNode(el) {
+  isHtmlNode(el) { // 如果是标签
     return el.nodeType === 1
   }
 }
